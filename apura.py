1ce1a96f3e981663b9804406fa2698b6bf09ebcb
arthurto <arthur.ps22009@gmail.com>
2022-10-02 17:35:04 -0300

    atualização

--- apura.py
+++ apura.py
@@ -7,17 +7,19 @@ import time
 url = "https://resultados.tse.jus.br/oficial/ele2022/544/dados-simplificados/br/br-c0001-e000544-r.json"
 
 while True:
-    os.system('cls' if os.name == 'nt' else 'clear')
+    # os.system('cls' if os.name == 'nt' else 'clear')
     # Pegando os dados
     resp = requests.get(url)
     resp = resp.json()
 
     # Separando os candidatos
     candidatos = resp['cand']
+    print(50*'-')
+    print(f" ------- TOTAL DE URNAS APURADAS {resp['pst']} -------")
+    print(50*'-')
     print(f"NOME {20*'-'} VOTOS % {10*'-'} VOTOS TOTAL")
     for candidato in candidatos:
         n_1 = 20 - len(candidato['nm'])
         n_2 = 10 
         print(f"{candidato['nm']}  {n_1*'-'}  {candidato['pvap']}  {n_2*'-'}  {candidato['vap']} ")
-    time.sleep(1)
-
+    time.sleep(0.5)
